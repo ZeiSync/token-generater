b9e823ebd492080e9a401b36c63eb0a491757fb4
zeisync <phamhoang.dev@gmail.com>
2023-04-28 17:09:44 +0700

    remove some code

--- ui/src/components/TokenForm.tsx
+++ ui/src/components/TokenForm.tsx
@@ -16,27 +16,6 @@ function TokenForm() {
     position: "bottom-right",
   });
 
-  useEffect(() => {
-    (async () => {
-      const { Mina, isReady, PublicKey } = await import("snarkyjs");
-      const { Add } = await import("../../../contracts/build/src/");
-
-      await isReady;
-
-      // Update this to use the address (public key) for your zkApp account.
-      // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
-      // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-      const zkAppAddress = "";
-      // This should be removed once the zkAppAddress is updated.
-      if (!zkAppAddress) {
-        console.error(
-          'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
-        );
-      }
-      //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
-    })();
-  }, []);
-
   function validate(value: string) {
     let error;
     if (!value) {
@@ -79,7 +58,7 @@ function TokenForm() {
               // const { Mina, isReady, PublicKey } = await import("snarkyjs");
               // const { TokenContract } = await import("../../../contracts/build/src/");
 
-							// const accounts = await minaWallet.getAccounts()
+              // const accounts = await minaWallet.getAccounts()
               // const tx = await Mina.transaction(() => {
               //   const TokenSmartContract = new TokenContract(
               //     accounts[0]
